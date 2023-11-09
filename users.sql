2ffa3b9a3540c8d7d86f0086d8b19b2a9f4bac84
VanDamDau555 <95405402+VanDamDau555@users.noreply.github.com>
2023-11-09 14:44:34 +0700

    users database - Son branch

--- users.sql
+++ users.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th10 08, 2023 lúc 05:59 AM
+-- Thời gian đã tạo: Th10 09, 2023 lúc 08:42 AM
 -- Phiên bản máy phục vụ: 10.4.28-MariaDB
 -- Phiên bản PHP: 8.2.4
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Cơ sở dữ liệu: `users`
+-- Cơ sở dữ liệu: `manager_products`
 --
 
 -- --------------------------------------------------------
@@ -42,26 +42,6 @@ INSERT INTO `users` (`id`, `user_name`, `password`, `name`) VALUES
 (1, 'nguyennhattung_t65@hus.edu.vn', 'admin', 'tung'),
 (2, 'ngodanghuy_t65@hus.edu.vn', 'admin', 'huy'),
 (3, 'nguyentrongson_t65@hus.edu.vn', 'admin', 'son');
-
---
--- Chỉ mục cho các bảng đã đổ
---
-
---
--- Chỉ mục cho bảng `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT cho các bảng đã đổ
---
-
---
--- AUTO_INCREMENT cho bảng `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
